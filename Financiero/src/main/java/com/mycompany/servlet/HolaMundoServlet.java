48718796724b097b2314c14b6bb692f7f6fa6847
Jose Diaz <jose.diaz@joedayz.pe>
2014-05-31 16:00:12 -0500

    Financiero con Spring

---
+++ Financiero/src/main/java/com/mycompany/servlet/HolaMundoServlet.java
@@ -0,0 +1,44 @@
+package com.mycompany.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundoServlet
+ */
+@WebServlet("/HolaMundoServlet")
+public class HolaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HolaMundoServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.print("<html>");
+		out.print("<body><h1>Veremos JSF</h1></body>");
+		out.print("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
