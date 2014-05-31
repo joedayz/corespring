56c241145f48ef81574af980a2820ad99d232dea
Jose Diaz <jose.diaz@joedayz.pe>
2014-05-31 16:57:11 -0500

    dependencia jsf

---
+++ Financiero/src/main/java/com/mycompany/util/CrearTablas.java
@@ -0,0 +1,11 @@
+package com.mycompany.util;
+
+import javax.persistence.Persistence;
+
+public class CrearTablas {
+
+	public static void main(String[] args){
+		Persistence.
+			createEntityManagerFactory("FinancieroPU");
+	}
+}
