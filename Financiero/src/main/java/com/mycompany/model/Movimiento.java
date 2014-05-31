f70a73f15daf9002ced8bec763c89ec0e7ec68ae
Jose Diaz <jose.diaz@joedayz.pe>
2014-05-31 16:16:25 -0500

    mapeando entidades

--- Financiero/src/main/java/com/mycompany/model/Movimiento.java
+++ Financiero/src/main/java/com/mycompany/model/Movimiento.java
@@ -1,5 +1,120 @@
 package com.mycompany.model;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class Movimiento {
 
+	private Long id;
+	private Persona persona;
+	private String descripcion;
+	private BigDecimal valor;
+	private TipoMovimiento tipo;
+	private Date fechaVencimiento;
+	private Date fechaPago;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Persona getPersona() {
+		return persona;
+	}
+	public void setPersona(Persona persona) {
+		this.persona = persona;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public TipoMovimiento getTipo() {
+		return tipo;
+	}
+	public void setTipo(TipoMovimiento tipo) {
+		this.tipo = tipo;
+	}
+	public Date getFechaVencimiento() {
+		return fechaVencimiento;
+	}
+	public void setFechaVencimiento(Date fechaVencimiento) {
+		this.fechaVencimiento = fechaVencimiento;
+	}
+	public Date getFechaPago() {
+		return fechaPago;
+	}
+	public void setFechaPago(Date fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((descripcion == null) ? 0 : descripcion.hashCode());
+		result = prime * result
+				+ ((fechaPago == null) ? 0 : fechaPago.hashCode());
+		result = prime
+				* result
+				+ ((fechaVencimiento == null) ? 0 : fechaVencimiento.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((persona == null) ? 0 : persona.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		result = prime * result + ((valor == null) ? 0 : valor.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movimiento other = (Movimiento) obj;
+		if (descripcion == null) {
+			if (other.descripcion != null)
+				return false;
+		} else if (!descripcion.equals(other.descripcion))
+			return false;
+		if (fechaPago == null) {
+			if (other.fechaPago != null)
+				return false;
+		} else if (!fechaPago.equals(other.fechaPago))
+			return false;
+		if (fechaVencimiento == null) {
+			if (other.fechaVencimiento != null)
+				return false;
+		} else if (!fechaVencimiento.equals(other.fechaVencimiento))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (persona == null) {
+			if (other.persona != null)
+				return false;
+		} else if (!persona.equals(other.persona))
+			return false;
+		if (tipo != other.tipo)
+			return false;
+		if (valor == null) {
+			if (other.valor != null)
+				return false;
+		} else if (!valor.equals(other.valor))
+			return false;
+		return true;
+	}
+	
+	
 }
--- Financiero/src/main/java/com/mycompany/model/Persona.java
+++ Financiero/src/main/java/com/mycompany/model/Persona.java
@@ -16,6 +16,35 @@ public class Persona {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		return true;
+	}
 	
 	
 }
