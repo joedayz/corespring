28d7cc5561cbb84fc9aaed7fad99dd432a12becd
Jose Diaz <jose.diaz@joedayz.pe>
2014-05-31 17:55:20 -0500

    binding html

--- Financiero/src/main/java/com/mycompany/controllers/NombresBean.java
+++ Financiero/src/main/java/com/mycompany/controllers/NombresBean.java
@@ -3,18 +3,32 @@ package com.mycompany.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.component.html.HtmlCommandButton;
+import javax.faces.component.html.HtmlInputText;
 
 @ManagedBean
-@RequestScoped
+@ApplicationScoped
 public class NombresBean {
 
 	private String nombre;
 	private List<String> nombres = new ArrayList<>();
 	
+	private HtmlInputText inputNombre;
+	private HtmlCommandButton botonAgregar;
+	
+	
 	public void agregar(){
+		
 		this.nombres.add(nombre);
+		
+		if(this.nombres.size()>4){
+			this.inputNombre.setDisabled(true);
+			this.botonAgregar.setDisabled(true);
+			this.botonAgregar.setValue("Muchos nombres asignados");
+		}
+		
 	}
 	
 	
