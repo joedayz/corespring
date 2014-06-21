f5c38ae0745863dfe014b4df5796311c7315d348
Jose Diaz <jose.diaz@joedayz.pe>
2014-06-21 17:39:17 -0500

    interceptor para transacciones

---
+++ Financiero/src/main/java/com/mycompany/interceptor/TransactionInterceptor.java
@@ -0,0 +1,56 @@
+package com.mycompany.interceptor;
+
+import java.io.Serializable;
+
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+
+@Interceptor
+@Transactional
+public class TransactionInterceptor 
+					implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private @Inject EntityManager manager;
+	
+	@AroundInvoke
+	public Object invoke(InvocationContext context)
+				throws Exception {
+		EntityTransaction trx = manager.getTransaction();
+		boolean creado = false;
+		try{
+			if(!trx.isActive()){
+				//truco para hacer rollback
+				//a operaciones que no se han hecho commit
+				trx.begin();
+				trx.rollback();
+
+				//ahora si inicia la transaction
+				trx.begin();
+				creado = true;
+			}
+			return context.proceed();
+		}catch (Exception ex){
+			if(trx!=null && creado){
+				trx.rollback();
+			}
+			throw e;
+		} finally{
+			if(trx!=null && trx.isActive() && creado){
+				trx.commit();
+			}
+		}
+	
+	}
+	
+
+}
