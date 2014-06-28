b287a65a9eb722b49113828fe6546efd2b04c396
Jose Diaz <jose.diaz@joedayz.pe>
2014-06-28 15:45:03 -0500

    PersonaConverter ahora usando CDI

--- Financiero/src/main/java/com/mycompany/controllers/PersonaConverter.java
+++ Financiero/src/main/java/com/mycompany/controllers/PersonaConverter.java
@@ -11,32 +11,31 @@ import com.mycompany.repository.PersonaRepository;
 import com.mycompany.util.JpaUtil;
 
 @FacesConverter(forClass = Persona.class)
-public class PersonaConverter 
-	implements Converter{
+public class PersonaConverter implements Converter {
+
+	// @Inject no es soportado en un Converter
+	private PersonaRepository personaRepository;
+
+	public PersonaConverter() {
+		personaRepository = CDILocator.getBean(PersonaRepository.class);
+	}
 
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component,
 			String value) {
 		Persona retorno = null;
-		EntityManager manager = JpaUtil.getEntityManager();
-		
-		try{
-			if (value!=null){
-				PersonaRepository repository =
-						new PersonaRepository(manager);
-				retorno = repository.porId(new Long(value));
-			}
-			return retorno;
-		}finally{
-			manager.close();
+
+		if (value != null) {
+			retorno = personaRepository.porId(new Long(value));
 		}
-	
+		return retorno;
+
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component,
 			Object value) {
-		if(value!=null)
+		if (value != null)
 			return ((Persona) value).getId().toString();
 		return null;
 	}
