e8528dbb1a70e48f99cf7b56be0f99fe0717d37c
Jose Diaz <jose.diaz@joedayz.pe>
2014-06-28 17:21:54 -0500

    login

--- Financiero/src/main/java/com/mycompany/controllers/LoginBean.java
+++ Financiero/src/main/java/com/mycompany/controllers/LoginBean.java
@@ -1,13 +1,42 @@
 package com.mycompany.controllers;
 
+import java.util.Date;
+
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+
+import com.mycompany.model.Usuario;
 
 @ManagedBean
 @RequestScoped
 public class LoginBean {
 
+	@Inject
+	private Usuario usuario;
+
+	private String nombreUsuario;
 	private String password;
+
+	public String login(){
+		FacesContext context = 
+				FacesContext.getCurrentInstance();
+		
+		if("admin".equals(this.nombreUsuario)
+		 && "123".equals(this.password)){
+			 this.usuario.setNombre(this.nombreUsuario);
+			 this.usuario.setFechaLogin(new Date());
+		 }else{
+			 FacesMessage mensaje = new FacesMessage(
+					 "Usuario/Password invalidos!");
+			 mensaje.setSeverity(FacesMessage.
+					 SEVERITY_ERROR);
+			 context.addMessage(null, mensaje);
+		 }
+		 return null;
+	}
 
 	public String getPassword() {
 		return password;
@@ -16,6 +45,13 @@ public class LoginBean {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
 }
---
+++ Financiero/src/main/java/com/mycompany/model/Usuario.java
@@ -0,0 +1,43 @@
+package com.mycompany.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class Usuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private String nombre;
+	private Date fechaLogin;
+	
+	public boolean isLogueado(){
+		return nombre !=null;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaLogin() {
+		return fechaLogin;
+	}
+
+	public void setFechaLogin(Date fechaLogin) {
+		this.fechaLogin = fechaLogin;
+	}
+	
+	
+	
+}
