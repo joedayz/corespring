ce18517f4f9d9b4e8a48698a833311b7ade0b2c4
Jose Diaz <jose.diaz@joedayz.pe>
2014-06-28 15:49:55 -0500

    Movimientoconverter

--- Financiero/src/main/java/com/mycompany/controllers/MovimientoConverter.java
+++ Financiero/src/main/java/com/mycompany/controllers/MovimientoConverter.java
@@ -6,29 +6,39 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
 import com.mycompany.model.Movimiento;
+import com.mycompany.repository.MovimientoRepository;
 
 @FacesConverter(forClass = Movimiento.class)
 public class MovimientoConverter implements 
 	Converter{
 
 	
-	private Movimiento movimientos;
+	private MovimientoRepository repository;
 
 	public MovimientoConverter(){
-		 
+		 this.repository = 
+				 CDILocator.
+				 getBean(MovimientoRepository.class);
 	}
 	
 	@Override
 	public Object getAsObject(FacesContext context, UIComponent component,
 			String value) {
-		// TODO Auto-generated method stub
-		return null;
+		Movimiento retorno = null;
+		if (value!=null){
+			retorno = this.repository.porId(new Long(value));
+		}
+		return retorno;
 	}
 
 	@Override
 	public String getAsString(FacesContext context, UIComponent component,
 			Object value) {
-		// TODO Auto-generated method stub
+		if(value!=null){
+			Movimiento movimiento = ((Movimiento) value);
+			return movimiento.getId() == null ? null:
+				movimiento.getId().toString();
+		}
 		return null;
 	}
 	
